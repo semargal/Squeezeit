db95f952e001aa93bb4c0c2cc4c53bc6817dcf95
s_kupr <semargal@gmail.com>
2017-01-05 15:16:21 +0200

    fix requirements

--- setup.py
+++ setup.py
@@ -5,16 +5,17 @@ import sys, os
  
 version = '0.8'
  
-setup(name='squeezeit',
-	version=version,
-	author='Sam Rudge',
-	author_email='sam@rmg.io',
-	packages=['squeezeit'],
-	include_package_data=True,
-	zip_safe=False,
-	entry_points="""
-	[console_scripts]
-	squeezeit = squeezeit.cli:start
-	""",
-	install_requires='pyyaml>=3.10'
-	)
+setup(
+    name='squeezeit',
+    version=version,
+    author='Sam Rudge',
+    author_email='sam@rmg.io',
+    packages=['squeezeit'],
+    include_package_data=True,
+    zip_safe=False,
+    entry_points="""
+    [console_scripts]
+    squeezeit = squeezeit.cli:start
+    """,
+    install_requires=['pyyaml>=3.10', 'jsmin>=2.2.1', 'slimmer>=0.1.30'],
+)
